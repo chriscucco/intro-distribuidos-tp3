3d30a50688135cc86c4849096d94b26d30f3136b
Christian Cucco <cdc-96@hotmail.com>
2021-06-26 18:43:13 -0300

    add calculate num of switches

--- custom-topology.py
+++ custom-topology.py
@@ -1,12 +1,20 @@
 from mininet.topo import Topo
+import math
 import sys
 
 class CustomTopo(Topo):
     def build(self, levels, hosts):
-        print('AAAAAAAAAAAAAAAA')
-        print(levels)
-        print(hosts)
-        print(sys.argv)
+        numOfSwitches = self.calculateNumOfSwitches(int(levels))
+        print('//////////////////////')
+        print(numOfSwitches)
+        print('//////////////////////')
         return
+
+    def calculateNumOfSwitches(self, levels):
+        sum = math.pow(2, levels - 1)
+        while ( 0 < levels -1):
+            sum += math.pow(2, levels - 1)
+            levels -= 1
+        return sum
 
 topos = { 'custom': (lambda x,y: CustomTopo(x,y))}

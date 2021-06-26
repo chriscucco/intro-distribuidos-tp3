081c209421739b0878fa17ff45f9814c79e70e97
Christian Cucco <cdc-96@hotmail.com>
2021-06-26 18:19:59 -0300

    fix params

--- custom-topology.py
+++ custom-topology.py
@@ -2,9 +2,11 @@ from mininet.topo import Topo
 import sys
 
 class CustomTopo(Topo):
-    def build(self):
+    def build(self, levels, hosts):
         print('AAAAAAAAAAAAAAAA')
+        print(levels)
+        print(hosts)
         print(sys.argv)
         return
 
-topos = { 'custom': (lambda x,y: CustomTopo())}
+topos = { 'custom': (lambda x,y: CustomTopo(x,y))}

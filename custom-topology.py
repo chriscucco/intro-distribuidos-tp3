57e3312d1cf69529e323703e11c9aa296377d289
Christian Cucco <cdc-96@hotmail.com>
2021-06-27 12:50:21 -0300

    hosts and switches creation

--- custom-topology.py
+++ custom-topology.py
@@ -2,10 +2,35 @@ from mininet.topo import Topo
 import math
 
 class CustomTopo(Topo):
-    def build(self, levels, hosts):
+    def build(self, levels, hostsNum):
         numOfSwitches = self.calculateNumOfSwitches(int(levels))
+        numOfLevels = int(levels)
+        numOfHosts = int(hostsNum)
+        switches = []
+        hosts = []
+
         print('//////////////////////')
-        print(numOfSwitches)
+        i = 1
+        while numOfSwitches > 0:
+            switchName = 's'+str(i)
+            print('Creating switch '+switchName)
+            s = self.addSwitch(switchName, failMode='standalone', stp=True)
+            switches.append(s)
+            numOfSwitches -= 1
+            i += 1
+        
+        i = 1
+        while numOfHosts > 0:
+            hostName = 'h'+str(i)
+            print('Creating host '+hostName)
+            h = self.addHost(hostName)
+            hosts.append(h)
+            numOfHosts -= 1
+            i += 1
+
+        print('Hosts and switches created')
+        print('Hosts size: '+str(len(hosts)))
+        print('Switches size: '+str(len(switches)))
         print('//////////////////////')
         return
 
@@ -14,6 +39,6 @@ class CustomTopo(Topo):
         while ( 0 < levels -1):
             sum += math.pow(2, levels - 1)
             levels -= 1
-        return sum
+        return int(sum)
 
 topos = { 'custom': (lambda x,y: CustomTopo(x,y))}

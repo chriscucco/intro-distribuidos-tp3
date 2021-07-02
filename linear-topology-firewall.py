7543d52d971fb5d6a57fc6c55dbbdfd32e6bbf84
Ramiro <ram.santos@hotmail.com>
2021-07-02 00:31:08 -0300

    firewall de prueba

--- linear-topology-firewall.py
+++ linear-topology-firewall.py
@@ -1,61 +1,25 @@
-from pox . core import core
-import pox . openflow . libopenflow_01 as of
-from pox . lib . revent import *
-from pox . lib . util import dpidToStr
-from pox . lib . addresses import EthAddr
-from collections import namedtuple
-import os
-# Add your imports here ...
-log = core . getLogger()
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.addresses import EthAddr
 
-HOST_1 = '00:00:00:00:00:01'
-HOST_2 = '00:00:00:00:00:02'
-HOST_3 = '00:00:00:00:00:03'
+rules = [['00:00:00:00:00:01','00:00:00:00:00:02'],['00:00:00:00:00:02', '00:00:00:00:00:04'],['00:00:00:00:00:08','00:00:00:00:00:03'],['00:00:00:00:00:07','00:00:00:00:00:02']]
 
 
-class Firewall (EventMixin):
+class SDNFirewall (EventMixin):
+
     def __init__(self):
-        self . listenTo(core.openflow)
-        log. debug(" Enabling ␣ Firewall ␣ Module ")
+        self.listenTo(core.openflow)
 
     def _handle_ConnectionUp(self, event):
-        # Regla 1: no se pueden comunicar host 2 y 3
-        blocked = of.ofp_match()
-        blocked.dl_src = EthAddr(HOST_2)
-        blocked.dl_dst = EthAddr(HOST_3)
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = blocked
-        event.connection.send(flow_mod)
-
-        # Regla 2: descartar paquetes con UDP, puerto 5001 y con src en host1
-        '''blocked = of.ofp_match()
-        blocked.dl_src = EthAddr(HOST_1)
-        blocked.tp_dst = 5001
-        blocked.new_proto = 17
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = blocked
-        event.connection.send(flow_mod)
-
-        # Regla 3: descartar paquetes con puerto 80
-        blocked = of.ofp_match()
-        blocked.tp_dst = 80
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = blocked
-        event.connection.send(flow_mod)'''
-
-        '''
-        packet = event.parsed
-        if packet.type == packet.IP_TYPE:
-            ip_packet = packet.payload
-            if ip_packet.protocol == ip_packet.17:
-
-        packet.dst = 80
-        '''
-
-        return
+        for rule in rules:
+            block = of.ofp_match()
+            block.dl_src = EthAddr(rule[0])
+            block.dl_dst = EthAddr(rule[1])
+            flow_mod = of.ofp_flow_mod()
+            flow_mod.match = block
+            event.connection.send(flow_mod)
 
 
 def launch():
-    # Starting the Firewall module
-    core.registerNew(Firewall)
-    # core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+    core.registerNew(SDNFirewall)
